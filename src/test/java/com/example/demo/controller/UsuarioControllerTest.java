3566662bcab2d7c0d8e27b84319900910052988c
samuel <pacomairena515@gmail.com>
2026-02-21 20:51:26 +0000

    Arregla tests y contextLoads

--- src/test/java/com/example/demo/controller/UsuarioControllerTest.java
+++ src/test/java/com/example/demo/controller/UsuarioControllerTest.java
@@ -1,21 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Usuario;
 import com.example.demo.repository.UsuarioRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UsuarioControllerTest {
 
     @Autowired
-    private UsuarioController usuarioController;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UsuarioRepository usuarioRepository;
 
     @Test
     void contextLoads() {
-        // Verifica que el controlador se haya cargado correctamente en el con-texto
-        assertThat(usuarioController).isNotNull();
+        // Este test solo verifica que el contexto de Spring arranca
     }
 }
