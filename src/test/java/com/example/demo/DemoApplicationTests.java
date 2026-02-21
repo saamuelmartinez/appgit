832ab50280713ef4c86a1e33e07c8dd71fb4ae80
saamuelmartinez <146060459+saamuelmartinez@users.noreply.github.com>
2026-02-21 22:07:00 +0100

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,3 +1,7 @@
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
